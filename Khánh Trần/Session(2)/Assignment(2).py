b819776f2bc119430b45c1866a2c3a510158cdf9
Khánh Trần <Khnh Trn>
2016-06-20 21:44:32 +0700

    no message

---
+++ Khánh Trần/Session(2)/Assignment(2).py
@@ -0,0 +1,5 @@
+##a program that calculates the area of a circle
+pi = 3.14
+r = int(input('Please enter the radius of the circle :'))
+s = pi*r*r
+print('Area = ',s)
---
+++ Khánh Trần/Session(2)/Assignment(3).py
@@ -0,0 +1,3 @@
+## a program that converts Celsius (0C) into Fahrenheit (0F) 
+celsius = float(input('Please enter the celcius degree: '))
+print(celsius,' (C) = ',celsius*(9/5)+32,' (F)')
