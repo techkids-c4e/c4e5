a370388de87d614e9c70e25903f851c97fe3957e
MiDuDeZa <drmaroon5@gmail.com>
2016-06-21 23:07:42 +0700

    Update Bài 4.py

--- Minh Đức/BTVN 19.6/Bài 4.py
+++ Minh Đức/BTVN 19.6/Bài 4.py
@@ -1,7 +1,7 @@
 B=int(input('Please enter the initial B bacteria number: '))
 time=float(input('Thank you! Now, please enter a period of time: '))
 if time%2==0:
-    print('Total number of B bacteria after this period is: ', time*B)
+    print('After ',time,'minutes we would have ',time*B,'bacterias')
 else:
     Time=time-1
     print('After ',time,'minutes we would have ',Time*B,'bacterias')
