0ddcd3bbf9010b36942822978d4554704805793a
hatp95 <tranphuongha2912@gmail.com>
2016-06-22 03:32:19 +0700

    no message

---
+++ Phuong Ha/BT_1906/B5.py
@@ -0,0 +1,13 @@
+## tinh tong so doi tho sau 5 tháng
+
+t0=1 #so doi tho thang dau
+t1=2 #so doi tho thang 2
+
+for i in range(5):
+    if (i<2):
+        print(str.format('Thang {0} co {1} doi tho',i,i+1))
+    if (i>1):
+        x=t1
+        t1=t0+t1
+        t0=x
+        print(str.format('Thang {0} co {1} doi tho',i,t1))
