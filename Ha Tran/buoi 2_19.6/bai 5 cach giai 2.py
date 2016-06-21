dc680c5772b2cdf46b6b4f6ff1e9ebe15ff39414
hatt1992 <hatt1992@gmail.com>
2016-06-21 10:56:14 +0700

    Update bai 5 cach giai 2.py

--- Ha Tran/buoi 2_19.6/bai 5 cach giai 2.py
+++ Ha Tran/buoi 2_19.6/bai 5 cach giai 2.py
@@ -1,7 +1,7 @@
 def fib(n):# write Fibonacci series up to n
-    if n<2:
-        return n+1
-    return fib(n-1)+fib(n-2)
-for month in range(5):
-    print('Month ',month,' :',fib(month),' pair(s) of rabbits')
+    if n<1: #thang 0 thi f(0) bang 1
+        return 1
+    return fib(n-1)+fib(n-2) #Cac thang con lai: so cap tho thang n bang tong so cap tho thang (n-1) va thang (n-2)
+for month in range(5):#Trong 4 thang dau tien
+    print('Month ',month,' :',fib(month),' pair(s) of rabbits')#Hien thi ket qua
     
