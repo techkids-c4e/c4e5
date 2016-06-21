eec6cc5d52da9e08845dfc33fbd6e0271b67ed49
hatt1992 <hatt1992@gmail.com>
2016-06-22 00:35:15 +0700

    Update bai 5.py

--- Ha Tran/buoi 2_19.6/bai 5.py
+++ Ha Tran/buoi 2_19.6/bai 5.py
@@ -2,7 +2,7 @@
 def fib(n):                             #Dinh nghia ham fib(n)
     baby,mature = 0,1                   #thang0: so cap tho con = 0, tong so cap tho = 1
 #Cong thuc tinh Pair tho thang thu n
-    for i in range(n):                
+    for i in range(n):                  #i chay tu 0 den n
         baby,mature = mature,baby + mature  #baby thang n=mature thang (n-1)
                                         #mature thang n = baby thang (n-1)+ mature thang (n-1)
     return mature+baby                  #Ra cong thuc fib(n): total thang n = mature thang n + baby thang n
