14f5264722116c0fa067391297b0e8f5f2a68776
hatt1992 <hatt1992@gmail.com>
2016-06-21 10:48:13 +0700

    Update bai 4.py

--- Ha Tran/buoi 2_19.6/bai 4.py
+++ Ha Tran/buoi 2_19.6/bai 4.py
@@ -1,4 +1,4 @@
-initial_number=int(input("Initial B bacteria number : "))
-time=float(input("Period of time : "))
-total_B=int(initial_number*(2**(time//2)))
-print("After ",time," minutes we would have",total_B," bacteria(s)")
+initial_number=int(input("Initial B bacteria number : ")) #Nhap so luong vi khuan ban dau
+time=float(input("Period of time : ")) #Nhap so phut
+total_B=int(initial_number*(2**(time//2))) #tinh tong so vi khuan
+print("After ",time," minutes we would have",total_B," bacteria(s)") #Hien thi ket qua
