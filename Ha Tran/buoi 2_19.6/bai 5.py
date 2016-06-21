ef3509a02c26967dda4a878619377788c1090071
hatt1992 <hatt1992@gmail.com>
2016-06-21 12:33:38 +0700

    Update bai 5.py

--- Ha Tran/buoi 2_19.6/bai 5.py
+++ Ha Tran/buoi 2_19.6/bai 5.py
@@ -5,7 +5,7 @@ def fib(n):                             #Dinh nghia ham fib(n)
     for i in range(n):                
         baby,mature=mature,baby+mature  #Pair tho con thang sau=pair tho lon thang truoc
                                         #pair tho lon thang sau = pair tho con thang truoc+ pair tho lon thang truoc
-    return mature+baby                  #Ra cong thuc fib(n)
+    return mature+baby                  #Ra cong thuc fib(n): pair tho thang sau = tho con thang sau + tho lon thang sau
 for i in range(5):                      #Trong 4 thang dau tien
     print("Month ",i,": ",fib(i)," pair(s) of rabbits") #Hien thi ket qua: so pair tho lon thang thu i
 
