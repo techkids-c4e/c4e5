893accdbd2f9dcdcd612207b73873a669e9ae5df
hatt1992 <hatt1992@gmail.com>
2016-06-25 18:38:47 +0700

    ...

---
+++ Ha Tran/buoi 3_22.6/Hinh 4.py
@@ -0,0 +1,8 @@
+from turtle import*
+speed(0)
+color('green')
+for ring in range (6):
+    for circle in range(360):
+        forward(1)
+        left(1)
+    left(60)
---
+++ Ha Tran/buoi 3_22.6/bai tap tren lop.py
@@ -0,0 +1,5 @@
+n =input('What is your name?')
+print('Hello',n)
+print(n,'is beautiful')
+print(n,'is sexy')
+print(n,'is out of this world')
