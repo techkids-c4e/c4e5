ee28f2ccea03666cf058ec720ffd58a0aff3e300
dinhle0709 <dinhle0709@users.noreply.github.com>
2016-06-28 14:43:50 +0000

    bai tap buoi 3

---
+++ bai tap 4.py
@@ -0,0 +1,8 @@
+from turtle import *
+d=360
+begin_fill()
+for i in range (d):
+    forward(1)
+    left(360/d)
+    color("green","yellow")
+end_fill()
---
+++ bai tap 5.py
@@ -0,0 +1,9 @@
+from turtle import*
+d=360
+speed(0)
+for i in range (d) :
+    for i in range (360):
+        color("green","black")
+        forward(1)
+        left(360/d)
+    left(360/30)
