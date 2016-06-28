74215da55ad663503b0edb1fc24a58813ee04707
dinhle0709 <levandinh321@gmail.com>
2016-06-29 00:34:40 +0700

    Sesson4

---
+++ Lê Văn Dịnh/buoi 5/bai1.py
@@ -0,0 +1,15 @@
+#draw_square
+from turtle import*
+def draw_square(length,color_):
+    color(color_)
+    for i in range(4):
+        forward(legth)
+        left(90)
+for i in range(30):
+    draw_square(i*5,'red')
+    left(17)
+    penup()
+    forward(i*2)
+    pendown()
+
+        
