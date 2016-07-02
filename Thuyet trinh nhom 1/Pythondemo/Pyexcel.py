bcd2ce1f8bd21bf7a13de75ae7fe58bcf67571f6
hatp95 <tranphuongha2912@gmail.com>
2016-07-02 19:34:26 +0700

    Nhom 1: PyXL

---
+++ Thuyet trinh nhom 1/Pythondemo/Pyexcel.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  2 10:58:08 2016
+
+@author: Hoang To
+"""
+
+from pyexcel import *
+from pyexcel-xlsx import *
+rp=get_records(file_name="D:\Pythondemo\chamcong.xlsx")
