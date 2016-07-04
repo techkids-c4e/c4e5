ecac99dd5b370613f5972a44101367ecc3441e8b
hatt1992 <hatt1992@gmail.com>
2016-07-04 21:51:09 +0700

    buoi 7_29.6

---
+++ Ha Tran/ham tinh toan.py
@@ -0,0 +1,16 @@
+x=float(input('Nhap so thu nhat:'))
+y=float(input('Nhap so thu hai:'))
+i=str(input('Nhap dau'))
+def calculator (x, i, y):
+    if i == '+':
+        answer = x + y
+    elif i == '-':
+        answer = x - y
+    elif i == '*':
+        answer = x * y
+    elif i == '/':
+        answer = x / y
+    return answer
+print(calculator(x, i, y))
+
+    
