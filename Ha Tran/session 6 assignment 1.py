4784c81c54fa23bd88fe0ba952a026c7695deb5b
hatt1992 <hatt1992@gmail.com>
2016-07-09 13:13:43 +0700

    ...

--- Ha Tran/session 6 assignment 1.py
+++ Ha Tran/session 6 assignment 1.py
@@ -8,20 +8,21 @@ haiduong = {"name" : "HaiDuong",
             "lat" : 10}
 cities=[hanoi,haiduong]
 class City:
-    def __init__(self,name,longt,lat):
-        self.n=name
-        self.lo=longt
-        self.la=lat
+    def __init__(self,city1):
+        self.n=city1["name"]
+        self.lo=city1["longt"]
+        self.la=city1["lat"]
 
-    def calculateDistance(self,city):
-        distance=(((city["longt"]-self.lo)**2+(city["lat"]-self.la)**2)**0.5)
-        print('The distance is: ',round(distance,2))
+    def calculateDistance(self,city2):
+        distance=(((city2["longt"]-self.lo)**2+(city2["lat"]-self.la)**2)**0.5)
+        distance=round(distance,2)
+        return distance
 
 
-Hanoi=City(hanoi["name"],hanoi["longt"],hanoi["lat"])
-Hanoi.calculateDistance(haiduong)
+Hanoi=City(hanoi)
+x=Hanoi.calculateDistance(haiduong)
+print('The distance is: ',x)
 #wave 2
-
 print('Hello, This is City Distance Caculate System.')
 name=input('Please, Enter your City name:')
 longt=input('Please, Enter your City’s longitude')
